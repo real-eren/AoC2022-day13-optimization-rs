ce8bb33232d0c6d31feb1c6b8eaab6057ad3e8cd
real-eren <erenalpkahriman@gmail.com>
2023-08-13 15:24:32 -0400

    improve line splitting algo

--- src/shared.rs
+++ src/shared.rs
@@ -1,24 +1,23 @@
 /// Dependency for day13 implementations.
-///
-/// The control could be inverted (all impls only export the line-compare closure),
-/// but ATM I don't see a drastically different alternative input parsing approach that would be
-/// interesting to benchmark
 use std::cmp::Ordering;
 
 /// Outline of a solution - extracts pairs and passes them to the given line comparator
 pub(crate) fn day13_framework(
-    input: &str,
+    mut input: &str,
     mut line_comparator: impl FnMut(&str, &str) -> Ordering,
 ) -> usize {
-    input
-        .split("\n\n")
-        .map(|chunk| {
-            chunk
-                .split_once('\n')
-                .unwrap_or_else(|| panic!("strange format: {chunk}"))
-        })
-        .map(|(l, r)| line_comparator(l, r))
-        .enumerate()
-        .filter_map(|(idx, ord)| if ord.is_lt() { Some(idx + 1) } else { None })
-        .sum()
+    let mut count = 0;
+    let mut idx = 1;
+    while !input.is_empty() {
+        let Some((left, rem)) = input.split_once('\n') else {break};
+        let (right, rem) = rem.split_once('\n').unwrap_or((rem, ""));
+
+        if line_comparator(left, right).is_lt() {
+            count += idx;
+        }
+
+        input = rem.trim_start_matches('\n');
+        idx += 1;
+    }
+    return count;
 }
